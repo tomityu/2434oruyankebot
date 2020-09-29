6d4a8e238df6de7e85e816fbad283ec0d37d99ca
tomityu <qucheat303@gmail.com>
2020-09-29 21:17:28 +0900

    temp

--- src/live_streaming_monitor.py
+++ src/live_streaming_monitor.py
@@ -14,16 +14,16 @@ def search_live_streaming(youtube_service, q, event_type, monitoring_list):
     logger.info(
         f"{datetime.now().strftime('%Y/%m/%d %H:%M:%S')} | {event_type} search start.")
     search_response = youtube_service.execute_search_list(q, event_type)
-    temp_monitoring_list = {}
+    temp_count = len(monitoring_list)
     for item in search_response['items']:
         if item['snippet']['channelId'] in channels:
             video_id = item['id']['videoId']
             try:
                 video_info = VideoInfo(video_id=video_id)
-                if is_valid_streaming(video_id, monitoring_list, temp_monitoring_list, video_info):
+                if is_valid_streaming(video_id, monitoring_list, video_info):
                     logger.info(
                         f"New {event_type}! | {video_id} | {item['snippet']['channelTitle']} | {item['snippet']['title']}")
-                    temp_monitoring_list[video_id] = {
+                    monitoring_list[video_id] = {
                         'channel': item['snippet']['channelTitle'], 'title': item['snippet']['title']}
                     pid = os.fork()
                     if pid == 0:  # No pid. so this is child process!
@@ -37,9 +37,9 @@ def search_live_streaming(youtube_service, q, event_type, monitoring_list):
                 logger.error(e)
                 logger.error(video_id)
                 continue
-    if temp_monitoring_list:
-        logger.info(f'{len(temp_monitoring_list)} new {event_type}s found!')
-        monitoring_list.update(temp_monitoring_list)
+    count = len(monitoring_list) - temp_count
+    if count:
+        logger.info(f'{count} new {event_type}s found!')
     else:
         logger.info(f'There was no new {event_type}.')
 
@@ -51,5 +51,5 @@ def print_monitoring_list(monitoring_list):
     logger.info(f'---{len(monitoring_list)} items---')
 
 
-def is_valid_streaming(video_id, monitoring_list, temp_monitoring_list, video_info):
-    return (not video_id in monitoring_list) and (not video_id in temp_monitoring_list) and (not video_id in exclude_video_ids) and (not video_info.get_duration())
+def is_valid_streaming(video_id, monitoring_list, video_info):
+    return (not video_id in monitoring_list) and (not video_id in exclude_video_ids) and (not video_info.get_duration())
